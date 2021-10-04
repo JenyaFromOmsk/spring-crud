d861906e187a0bd58b61cd9b36e9d1d7e9e56004
JenyaFromOmsk <90650551+JenyaFromOmsk@users.noreply.github.com>
2021-10-04 16:22:06 +0600

    Update UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @GetMapping
     public String showAllUsers(Model model) {
         model.addAttribute("allUsers", userService.allUsers());
         return "allUsers";
